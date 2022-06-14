0dc491b4fa4acd27a2fe2e7ef6402770994ca5aa
Liane Hampe <liaham@xmera.de>
2022-06-14 15:51:16 +0200

    Fix typo

--- init.rb
+++ init.rb
@@ -21,7 +21,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-Redmine::Plugin.register :redmine_dmsf doq
+Redmine::Plugin.register :redmine_dmsf do
   if Redmine::Plugin.installed?(:easy_extensions)
     name 'Easy DMS'
     url 'https://www.easyredmine.com'
