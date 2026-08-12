24954c4a9ce71d59e25c6165dbf3697f9ae1837e
Karel Picman <karel.picman@kontron.com>
2026-08-12 11:11:12 +0200

    folder 'changed date' does not change when files get new versions #38

--- app/models/dmsf_file_revision.rb
+++ app/models/dmsf_file_revision.rb
@@ -437,7 +437,7 @@ class DmsfFileRevision < ApplicationRecord
   # Update the parent folder's timestamp (updated_at)
   # Rails/SkipsModelValidations: Avoid using touch because it skips validations. =>
   # rubocop:disable Rails/SkipsModelValidations
-  def update_parent
+  def update_parent_folder
     dmsf_file.dmsf_folder&.touch time: updated_at
   end
   # rubocop:enable Rails/SkipsModelValidations
