f46ffb09b8356af0f9a97faa5c97e4bd2ae559ba
Karel Picman <karel.picman@kontron.com>
2026-08-19 08:21:12 +0200

    v5.1.2 devel started

--- init.rb
+++ init.rb
@@ -27,7 +27,7 @@ Redmine::Plugin.register :redmine_dmsf do
   author_url 'https://github.com/picman/redmine_dmsf/graphs/contributors'
   author 'Vít Jonáš / Daniel Munn / Karel Pičman'
   description 'Document Management System Features'
-  version '5.1.1'
+  version '5.1.2 devel'
 
   requires_redmine version_or_higher: '7.0.0'
 
